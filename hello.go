31f8a9c262d00f11a0416c769027bb46c38fcf6d
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2023-02-12 21:56:56 -0800

    Setup basic IO

--- hello.go
+++ hello.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
 
+func getFormattedUrl() (string, error) {
+	var inputReader *bufio.Reader
+	var input string
+	var formattedInput string
+	var err error
+
+	inputReader = bufio.NewReader(os.Stdin)
+	fmt.Printf("What Website do you want to scan?\n")
+	input, err = inputReader.ReadString('\n')
+
+	if err == nil {
+		formattedInput = strings.ReplaceAll(input, "\n", "/robots.txt")
+		return "https://" + formattedInput, nil
+	}
+	return "", err
+}
 func main() {
-	fmt.Printf("Hello, World!")
+	url, err := getFormattedUrl()
+	if err != nil {
+		fmt.Printf("Fatal Error in formatting that url properly: %s", err)
+		return
+	}
+	robotsFile, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Fatal Error in Retrieving that URL's Robots.txt file: %s", err)
+	}
+	fmt.Printf("Robots file: %s", robotsFile.Body)
+
 }
